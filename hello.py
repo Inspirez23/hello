7a9452236e0307e3ba8a9c2e0233e4a199448f99
Inspirez23 <dannytam23@gmail.com>
2025-03-31 20:59:45 -0400

    First commit

--- hello.py
+++ hello.py
@@ -13,14 +13,23 @@ def hello_world(request):
     return "hello world"
 
 @mcp.tool()
-async def adding(num1: int, num2: int) -> int:
-    """ Add two numbers together 
+async def dda(num1: int, num2: int) -> int:
+    """ dda two numbers together 
     Args:
         num1: first number
         num2: second number
     """
     # Return the sum of the two numbers
     return num1 + num2
+
+@mcp.tool()
+async def sunim(num1: int, num2: int) -> int:
+    """ sunim two numbers together 
+    Args:
+        num1: first number
+        num2: second number
+    """
+    return num1 - num2
 
 if __name__ == "__main__":
     # Run the server (defaults to host "127.0.0.1" and port 8000)
